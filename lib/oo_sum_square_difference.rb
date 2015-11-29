385cc15b11b8770e5d3320cdec887c681f8b9544
yoshikot <ytamaoki@gmail.com>
2015-11-29 10:39:05 -0800

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,34 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference
+
+  def initialize(input)
+    @input = input
+  end
+
+  def difference
+    square_sum - sum_squares
+  end
+
+  def sum_squares
+    squared_array = []
+    input_array.each {|item| squared_array << item ** 2}
+    squared_array.inject(:+)
+  end
+
+  def square_sum
+    input_array.inject(:+) ** 2
+  end
+
+  def input_array
+    input_array = Array.new(@input,1)
+    i = 1
+    while i < @input
+      input_array[i] = input_array[i - 1] + 1
+      i += 1
+    end
+    input_array
+  end
+
+end
+
+# SumSquareDifference.new(5).difference
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,26 @@
 # Implement your procedural solution here!
+def sum_square_difference(input)
+  square_sum(input) - sum_squares(input)
+end
+
+def sum_squares(input)
+  squared_array = []
+  input_array(input).each {|item| squared_array << item ** 2}
+  squared_array.inject(:+)
+end
+
+def square_sum(input)
+  input_array(input).inject(:+) ** 2
+end
+
+def input_array(input)
+  input_array = Array.new(input,1)
+  i = 1
+  while i < input
+    input_array[i] = input_array[i - 1] + 1
+    i += 1
+  end
+  input_array
+end
+
+# print sum_square_difference(10)
